2acb912d03f1ad07605f74dff068f252ff512051
Aurelien <ademaille@gmail.fr>
2025-09-10 10:58:56 +0200

    Renommage des classes

--- Classe/Vehicule.class.php
+++ Classe/Vehicule.class.php
@@ -35,3 +35,17 @@ class Vehicule {
         $this->poids = $this->poids + $poids_personne;
     }
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice - PhP en orienté objet</title>
+</head>
+<body>
+    
+</body>
+</html>
